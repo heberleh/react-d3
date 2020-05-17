3d40d82de38ec50283b2e4ceea6a3f644336b75d
Henry Heberle <heberleh.public@gmail.com>
2020-05-16 21:19:02 -0400

    Rendering histogram with axis

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
-import "./App.css";
-
-import Preloader from "./components/Preloader";
 import * as d3 from "d3";
 import _ from "lodash";
+
+import "./style.css";
+
+import Preloader from "./components/Preloader";
 import { loadAllData } from "./DataHandling";
+
 import CountyMap from "./components/CountyMap";
+import Histogram from "./components/Histogram";
 
 class App extends Component {
   state = {
     techSalaries: [],
+    medianIncomes: [],
+    countyNames: [],
   };
 
   componentDidMount() {
@@ -31,14 +36,14 @@ class App extends Component {
       value: median - medianHousehold.medianIncome,
     };
   }
-  render() {
-    const { techSalaries, countyNames, usTopoJson } = this.state;
 
-    if (techSalaries < 1) {
+  render() {
+    const { techSalaries, countyNames, usTopoJson, USstateNames } = this.state;
+
+    if (techSalaries.length < 1) {
       return <Preloader />;
     }
 
-    // Insert the line(s) between here...
     const filteredSalaries = techSalaries,
       filteredSalariesMap = _.groupBy(filteredSalaries, "countyID"),
       countyValues = countyNames
@@ -47,17 +52,28 @@ class App extends Component {
 
     return (
       <div className="App container">
-        <h1>Load {techSalaries.length} salaries.</h1>
+        <h1>Loaded {techSalaries.length} salaries</h1>
         <svg width="1100" height="500">
           <CountyMap
             usTopoJson={usTopoJson}
-            USstateName={USstateNames}
+            USstateNames={USstateNames}
             values={countyValues}
             x={0}
             y={0}
             width={500}
             height={500}
-            zoom={1}
+            zoom={null}
+          />
+          <Histogram
+            bins={10}
+            width={500}
+            height={500}
+            x={500}
+            y={10}
+            data={filteredSalaries}
+            axisMargin={83}
+            bottomMargin={5}
+            value={(d) => d.base_salary}
           />
         </svg>
       </div>
---
+++ src/components/CountyMap/CountyMap.js
@@ -0,0 +1,97 @@
+import React, { useMemo } from "react";
+import * as d3 from "d3";
+import * as topojson from "topojson";
+import _ from "lodash";
+
+import County from "./County";
+
+function useQuantize(values) {
+  return useMemo(
+    () =>
+      d3
+        .scaleQuantize()
+        .range(d3.range(9))
+        .domain([
+          d3.quantile(values, 0.15, (d) => d.value),
+          d3.quantile(values, 0.85, (d) => d.value),
+        ]),
+    [values]
+  );
+}
+
+function useGeoPath({ width, height, zoom, usTopoJson, USstateNames }) {
+  return useMemo(() => {
+    let projection = d3.geoAlbersUsa().scale(1280);
+    let geoPath = d3.geoPath().projection(projection);
+
+    projection.translate([width / 2, height / 2]).scale(width * 1.3);
+
+    if (zoom && usTopoJson) {
+      const us = usTopoJson,
+        USstatePaths = topojson.feature(us, us.objects.states).features,
+        id = _.find(USstateNames, { code: zoom }).id;
+
+      projection.scale(width * 4.5);
+
+      const centroid = geoPath.centroid(_.find(USstatePaths, { id: id })),
+        translate = projection.translate();
+
+      projection.translate([
+        translate[0] - centroid[0] + width / 2,
+        translate[1] - centroid[1] + height / 2,
+      ]);
+    }
+
+    return geoPath;
+  }, [width, height, zoom, usTopoJson, USstateNames]);
+}
+
+function CountyMap({ width, height, usTopoJson, values, zoom, USstateNames }) {
+  const quantize = useQuantize(values);
+
+  const geoPath = useGeoPath({
+    width,
+    height,
+    zoom,
+    usTopoJson,
+    USstateNames,
+  });
+
+  if (!usTopoJson) {
+    return null;
+  } else {
+    const us = usTopoJson,
+      USstatesMesh = topojson.mesh(us, us.objects.states, (a, b) => a !== b),
+      counties = topojson.feature(us, us.objects.counties).features;
+
+    const countyValueMap = _.fromPairs(
+      values.map((d) => [d.countyID, d.value])
+    );
+
+    return (
+      <g>
+        {counties.map((feature) => (
+          <County
+            geoPath={geoPath}
+            feature={feature}
+            zoom={zoom}
+            key={feature.id}
+            quantize={quantize}
+            value={countyValueMap[feature.id]}
+          />
+        ))}
+
+        <path
+          d={geoPath(USstatesMesh)}
+          style={{
+            fill: "none",
+            stroke: "#fff",
+            strokeLineJoin: "round",
+          }}
+        />
+      </g>
+    );
+  }
+}
+
+export default CountyMap;
